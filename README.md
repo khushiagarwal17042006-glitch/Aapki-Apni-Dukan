8892b44b8a53843664782093c5439459044ad9b5
Khushi Agarwal <khushiagarwal17@Khushis-MacBook-Air.local>
2026-03-25 19:39:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Aapki Apni Dukan
+
+A responsive e-commerce website made using HTML, CSS, and JavaScript. It fetches product data from an API and includes features like search, add to cart, quantity management, and saving the cart using localStorage.
+
+## Features
+
+- Product listing from API  
+- Search products  
+- Add to Cart  
+- Quantity management  
+- Remove items from cart  
+- Cart saved using localStorage  
+
+## Tech Used
+
+- HTML  
+- CSS  
+- JavaScript  
+
+## How to Run
+
+1. Download the project  
+2. Open index.html in browser  
+
+## Author
+
+- Khushi Agarwal
---
+++ script.js
@@ -0,0 +1,113 @@
+let productList = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+// Fetch products from API
+async function getProducts() {
+    try {
+        let response = await fetch("https://fakestoreapi.com/products");
+        let data = await response.json();
+        productList = data;
+        showProducts(productList);
+    } catch (error) {
+        console.log("Error fetching data:", error);
+    }
+}
+// Display products on screen
+function showProducts(products) {
+    let container = document.getElementById("product-container");
+    container.innerHTML = "";
+
+    products.forEach(product => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <div class="card-content">
+                <img src="${product.image}" />
+                <h3>${product.title}</h3>
+                <p><b>$${product.price}</b></p>
+                <p>${product.category}</p>
+            </div>
+
+            <button onclick="addToCart(${product.id})">Add to Cart</button>
+        `;
+
+        container.appendChild(card);
+    });
+}
+// Call the function
+getProducts();
+function addToCart(id) {
+    let product = productList.find(item => item.id === id);
+
+    let existing = cart.find(item => item.id === id);
+
+    if (existing) {
+        existing.quantity += 1;
+    } else {
+        product.quantity = 1;
+        cart.push(product);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    updateCartCount();
+    showCart();
+}
+
+function updateCartCount() {
+    let count = 0;
+
+    cart.forEach(item => {
+        count += item.quantity;
+    });
+
+    document.getElementById("cart-count").innerText = count;
+}
+updateCartCount();
+
+function showCart() {
+    let container = document.getElementById("cart-items");
+    let total = 0;
+    container.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        total += item.price * item.quantity;
+
+        let div = document.createElement("div");
+        div.classList.add("cart-item");
+
+        div.innerHTML = `
+        <span>
+    ${item.title} - $${item.price} &nbsp; × &nbsp; <span class="qty">${item.quantity}</span>
+</span>
+        <button onclick="removeFromCart(${index})">Remove</button>
+        `;
+
+        container.appendChild(div);
+    });
+
+    document.getElementById("total-price").innerText = "Total: $" + total.toFixed(2);
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartCount();
+    showCart();
+}
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", function () {
+    let searchValue = searchInput.value.toLowerCase();
+
+    let filteredProducts = productList.filter(product =>
+        product.title.toLowerCase().includes(searchValue)
+    );
+
+    showProducts(filteredProducts);
+});
+
+updateCartCount();
+showCart();
